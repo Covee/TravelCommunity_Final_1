b4c3e307234598e2607f5ca91e64c03550a4d983
Covee <uniqtop91@gmail.com>
2017-11-03 18:08:32 +0900

    클라이언트에서 댓글 달 수 있는 template 구현

--- board/views.py
+++ board/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, View, TemplateView
-from .models import Post
+from .models import Post, Comment
 
 
 class PostList(ListView):
@@ -22,6 +22,18 @@ class KoreaBoard(ListView):
 
 
 
+def add(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('board.korea.post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'board.korea.post_detail', {'form': form})
 
 
 
